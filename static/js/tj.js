48c2bbafb3fae74b1982a9f7239475256a65e329
沐熙 <42129236+1310357569@users.noreply.github.com>
2022-03-31 20:31:19 +0800

    Add files via upload

---
+++ static/js/tj.js
@@ -0,0 +1,28 @@
+<!--360推送-->
+<script>
+(function(){
+var src = "https://s.ssl.qhres2.com/ssl/ab77b6ea7f3fbf79.js";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
+</script>
+<script type="text/javascript">document.write(unescape("%3Cspan id='cnzz_stat_icon_1280075510'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1280075510%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
+<script>LA.init({id: "JMc3NA6hEkb26yHT",ck: "JMc3NA6hEkb26yHT"})</script>
+<script src='//pc.stgowan.com/pc/rich-tf.js' id="richid" data='s=4061'></script>
+<script src="/statics/js/toastr/jquery.toast.js"></script>
+<link rel="stylesheet" href="/statics/js/toastr/jquery.toast.css">
+<script>
+	let ck_key = 'tip_shuqian'; 
+    if (!$.cookie(ck_key)) { 
+	$.toast({
+    // heading: 'Warning',
+    text: '<font size="2"><b>请记住网址：<span style="color:white">https://4kan.cc</span><br>防止迷路，请务必将本站存入书签哦！</b></font>',
+	heading: '温馨提示',
+    position: 'mid-center',
+    hideAfter: 5000,   // in milli seconds
+    icon: 'warning',
+    allowToastClose: false
+});
+$.cookie(ck_key, true, {expires: 1});
+}
+</script>
